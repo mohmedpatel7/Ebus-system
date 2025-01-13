60aba8555e32582b0ea1ffb442ae7178f6d84b72
mohmedpatel7 <153270796+mohmedpatel7@users.noreply.github.com>
2025-01-13 19:30:35 +0530

    dist updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure this is where you want the build output
+    rollupOptions: {
+      // Include specific optimizations or exclusions if needed
+      output: {
+        manualChunks: {
+          // Example of chunk splitting
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
