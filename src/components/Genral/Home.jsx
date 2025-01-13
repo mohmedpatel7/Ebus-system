968a124764f8698cf83f61ca60e379fe714630e3
mohmedpatel7 <153270796+mohmedpatel7@users.noreply.github.com>
2025-01-13 20:01:08 +0530

    updated

--- src/components/Genral/Home.jsx
+++ src/components/Genral/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
             Your trusted partner in bus transportation. Book tickets, view
             schedules, and enjoy seamless travel with us.
           </p>
-          {!isAdmin && !isUser && (
+          {isAdmin && isUser && (
             <button
               className="btn btn-light"
               onClick={() => {
